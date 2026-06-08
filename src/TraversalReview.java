1b7610d8ee390fe33b135609930b7d7c81135f88
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-06-08 11:12:48 -0700

    Completed positiveSum() method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -53,7 +53,15 @@ public class TraversalReview {
      * @return the sum of the positive values in the tree
      */
     public static int positiveSum(TreeNode node) {
-        return -1;
+        if(node == null) return 0;
+        int nLeft = positiveSum(node.left);
+        int nRight = positiveSum(node.right);
+        int nCenter = 0;
+        if(node.data > 0)
+        {
+            nCenter = node.data;
+        }
+        return nCenter + nLeft + nRight;
     }
 
     /**
